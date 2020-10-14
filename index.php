a7e5b9803400cf2ca9fd7f23e5591844d16712f9
saama143 <44967452+saama143@users.noreply.github.com>
2020-10-14 05:05:23 +0500

    Create index.php
    Add for loop with checking if number is multiple of 3 or 5 or both. else print the number

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+    for($i=1;$i<=100;$i++)
+    {
+        if( ($i%3==0) && ($i%5==0) ) {
+           echo 'FizzBuzz';
+        }
+        else if($i%3==0){
+           echo 'Fizz';
+        }
+        else if($i%5==0) {
+           echo 'buzz';
+        }
+        else {
+           echo $i;
+        }
+    }   
+?>
